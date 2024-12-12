9bb9b4cc78c042ed7f45880614e752274fde0304
BOY4ik <90923847+boy4ik7@users.noreply.github.com>
2024-12-12 18:17:59 +0200

    Update dice_tg_bot.ino

--- dice_tg_bot/dice_tg_bot.ino
+++ dice_tg_bot/dice_tg_bot.ino
@@ -16,6 +16,7 @@ BearSSL::WiFiClientSecure client;
 String randomNum;
 
 void updateh(fb::Update& u) {
+  digitalWrite(LED_BUILTIN, LOW);
   if ((u.message().text() == "/d4") || (u.message().text() == "/d4@lucy_dice_bot")) {
     randomNum = String(get_random_num(1, 4));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
@@ -45,6 +46,7 @@ void updateh(fb::Update& u) {
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
   }
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void setup() {
@@ -59,11 +61,31 @@ void setup() {
   bot.attachUpdate(updateh);
   bot.setToken(F(BOT_TOKEN));
   bot.setPollMode(fb::Poll::Long, 20000);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
   bot.tick();
 }
+int get_random_num(int minVal, int maxVal) {
+  client.setInsecure();
+  String url = "/api/v1.0/random?min=" + String(minVal) + "&max=" + String(maxVal) + "&count=1";
+  ghttp::Client http(client, "www.randomnumberapi.com", 443);
+  if (http.request(url)) {
+    ghttp::Client::Response resp = http.getResponse();
+    if (resp) {
+      String randomNum = resp.body().readString();
+      Serial.println("Response: " + randomNum);
+      int num = randomNum.substring(1, randomNum.length() - 1).toInt();
+      Serial.println("Parsed Number: " + String(num));
+      return num;
+    }
+  }
+  return -1;
+}
+//спустя сутки стал поятоянно давать ответ - 0
+/*
 int get_random_num(int minVal, int maxVal) {
   client.setInsecure();
   String url = "/integers/?num=1&min=" + String(minVal) + "&max=" + String(maxVal) + "&col=1&base=10&format=plain&rnd=new";
@@ -73,8 +95,10 @@ int get_random_num(int minVal, int maxVal) {
     if (resp) {
       String randomNum = resp.body().readString();
       int num = randomNum.toInt();
+      Serial.println(num);
       return num;
     }
   }
   return -1;
 }
+*/
