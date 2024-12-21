b765bf1c947a2a7b872207cde4777fb87be86887
BOY4ik <90923847+boy4ik7@users.noreply.github.com>
2024-12-21 16:07:25 +0200

    Update dice_tg_bot.ino

--- dice_tg_bot/dice_tg_bot.ino
+++ dice_tg_bot/dice_tg_bot.ino
@@ -3,6 +3,7 @@
 #define WIFI_SSID "" // имя вай-фай сети
 #define WIFI_PASS "" // пароль
 #define BOT_TOKEN "" // токен бота
+#define BOT_NAME "@" // имя бота @bot
 #define CHAT_ID "" // оставить пустым
 
 #include <FastBot2.h>
@@ -17,7 +18,7 @@ String randomNum;
 
 void updateh(fb::Update& u) {
   digitalWrite(LED_BUILTIN, LOW);
-  if ((u.message().text() == "/d4") || (u.message().text() == "/d4@lucy_dice_bot")) {
+  if ((u.message().text() == "/d4") || (u.message().text() == "/d4" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 4));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
@@ -25,23 +26,23 @@ void updateh(fb::Update& u) {
     randomNum = String(get_random_num(1, 6));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
-  } else if ((u.message().text() == "/d8") || (u.message().text() == "/d8@lucy_dice_bot")) {
+  } else if ((u.message().text() == "/d8") || (u.message().text() == "/d8" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 8));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
-  } else if ((u.message().text() == "/d10") || (u.message().text() == "/d10@lucy_dice_bot")) {
+  } else if ((u.message().text() == "/d10") || (u.message().text() == "/d10" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 10));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
-  } else if ((u.message().text() == "/d12") || (u.message().text() == "/d12@lucy_dice_bot")) {
+  } else if ((u.message().text() == "/d12") || (u.message().text() == "/d12" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 12));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
-  } else if ((u.message().text() == "/d20") || (u.message().text() == "/d20@lucy_dice_bot")) {
+  } else if ((u.message().text() == "/d20") || (u.message().text() == "/d20" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 20));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
-  } else if ((u.message().text() == "/d100") || (u.message().text() == "/d100@lucy_dice_bot")) {
+  } else if ((u.message().text() == "/d100") || (u.message().text() == "/d100" + String(BOT_NAME))) {
     randomNum = String(get_random_num(1, 100));
     if (randomNum == "-1") bot.sendMessage(fb::Message("Попробуйте ещё раз.", u.message().chat().id()));
     else bot.sendMessage(fb::Message(randomNum, u.message().chat().id()));
@@ -62,7 +63,6 @@ void setup() {
   bot.setToken(F(BOT_TOKEN));
   bot.setPollMode(fb::Poll::Long, 20000);
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
@@ -76,15 +76,13 @@ int get_random_num(int minVal, int maxVal) {
     ghttp::Client::Response resp = http.getResponse();
     if (resp) {
       String randomNum = resp.body().readString();
-      Serial.println("Response: " + randomNum);
       int num = randomNum.substring(1, randomNum.length() - 1).toInt();
-      Serial.println("Parsed Number: " + String(num));
       return num;
     }
   }
   return -1;
 }
-//спустя сутки стал поятоянно давать ответ - 0
+//спустя сутки стал постоянно давать ответ - 0
 /*
 int get_random_num(int minVal, int maxVal) {
   client.setInsecure();
